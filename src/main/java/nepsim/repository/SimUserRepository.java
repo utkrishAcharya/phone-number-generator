7c36f28eb1608bb6f57a1fbe7b136143cefc1518
utkrishAcharya <acharyautkrish14@gmail.com>
2026-02-08 08:16:13 +0545

    Add findBySimNumber method to SimUserRepository

--- src/main/java/nepsim/repository/SimUserRepository.java
+++ src/main/java/nepsim/repository/SimUserRepository.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
+    
 public interface SimUserRepository extends MongoRepository<SimUser, String> {
 
     // Find user by SIM number (unique)
