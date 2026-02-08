97ab3236123b4bdd9a49ab7b3375383832e9b447
utkrishAcharya <acharyautkrish14@gmail.com>
2026-02-08 08:16:28 +0545

    Add method to find user by username

--- src/main/java/nepsim/repository/UserRepository.java
+++ src/main/java/nepsim/repository/UserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.userdetails.User;
 import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User, String> {
+    
     Optional<User> findByUsername(String username);
 
 }
