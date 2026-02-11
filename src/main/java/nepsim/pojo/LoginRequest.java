40f373057c16793e42ca1617aa561a0480ecfe11
utkrishAcharya <acharyautkrish14@gmail.com>
2026-02-11 17:28:06 +0545

    Add password field to LoginRequest class

--- src/main/java/nepsim/pojo/LoginRequest.java
+++ src/main/java/nepsim/pojo/LoginRequest.java
@@ -3,11 +3,14 @@ package nepsim.pojo;
 public class LoginRequest {
 
     private String phoneNumber;
+    
     private String password;
 
     public String getPhoneNumber() { return phoneNumber; }
+    
     public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
 
     public String getPassword() { return password; }
+    
     public void setPassword(String password) { this.password = password; }
 }
