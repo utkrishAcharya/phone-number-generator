c6ddfd36c2fd13d9d90893c7ef11c181d3b59e02
utkrishAcharya <acharyautkrish14@gmail.com>
2026-02-12 14:33:00 +0545

    Fix formatting in SecurityConfig.java

--- src/main/java/nepsim/configuration/SecurityConfig.java
+++ src/main/java/nepsim/configuration/SecurityConfig.java
@@ -11,11 +11,9 @@ public class SecurityConfig {
     @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-       
-                // disable CSRF (otherwise POST requests like login/signup will be blocked) 
+     // disable CSRF (otherwise POST requests like login/signup will be blocked) 
             .csrf(csrf -> csrf.disable())
             
-
                 // permit all requests (so nothing requires authentication yet) 
             .authorizeHttpRequests(auth -> auth
             .anyRequest().permitAll();                        
