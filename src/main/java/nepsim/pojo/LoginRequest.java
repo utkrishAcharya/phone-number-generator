a647a765756c9147fb5cceba206e091dbe9bd181
utkrishAcharya <acharyautkrish14@gmail.com>
2026-02-12 14:34:28 +0545

    Refactor getters for phoneNumber and password

--- src/main/java/nepsim/pojo/LoginRequest.java
+++ src/main/java/nepsim/pojo/LoginRequest.java
@@ -3,14 +3,10 @@ package nepsim.pojo;
 public class LoginRequest {
 
     private String phoneNumber;
-    
     private String password;
-
-    public String getPhoneNumber() { return phoneNumber; }
-    
+ public String getPhoneNumber() { return phoneNumber; }
     public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
-
-    public String getPassword() { return password; }
+public String getPassword() { return password; }
     
     public void setPassword(String password) { this.password = password; }
 }
