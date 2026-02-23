8fae9d6c1dba0ed405dfb07e1ecda4c8962f2ead
utkrishAcharya <acharyautkrish14@gmail.com>
2026-02-23 18:07:54 +0545

    Update UserRepository.java

--- src/main/java/nepsim/repository/UserRepository.java
+++ src/main/java/nepsim/repository/UserRepository.java
@@ -2,7 +2,6 @@ package nepsim.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.security.core.userdetails.User;
-
 import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User, String> {
