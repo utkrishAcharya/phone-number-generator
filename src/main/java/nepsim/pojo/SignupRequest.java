e728761643f54aba574205bfa6eaa465da0f2c0d
utkrishAcharya <acharyautkrish14@gmail.com>
2026-03-01 21:50:12 +0545

    Update SignupRequest.java

--- src/main/java/nepsim/pojo/SignupRequest.java
+++ src/main/java/nepsim/pojo/SignupRequest.java
@@ -60,7 +60,7 @@ public class SignupRequest {
 
     public String getBirthPlace() { return birthPlace; }
     
-    public void setBirthPlace(String birthPlace) { this.birthPlace = birthPlace; }
+     public void setBirthPlace(String birthPlace) { this.birthPlace = birthPlace; }
     
 }
 
