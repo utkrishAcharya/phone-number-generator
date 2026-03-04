0c5137e4f04a4c8c9ca15b32af12f0e5a6cbb1a9
utkrishAcharya <acharyautkrish14@gmail.com>
2026-03-04 20:35:52 +0545

    Update SignupRequest.java

--- src/main/java/nepsim/pojo/SignupRequest.java
+++ src/main/java/nepsim/pojo/SignupRequest.java
@@ -39,6 +39,7 @@ public class SignupRequest {
     public String getPassword() { return password; }
     
     public void setPassword(String password) { this.password = password; }
+    
 
     public String getFatherName() { return fatherName; }
     
