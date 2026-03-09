c870c73ffae64e4affd96b2c61f7b9ab63141c10
utkrishAcharya <acharyautkrish14@gmail.com>
2026-03-09 21:48:26 +0545

    Update SimUserRepository.java

--- src/main/java/nepsim/repository/SimUserRepository.java
+++ src/main/java/nepsim/repository/SimUserRepository.java
@@ -15,6 +15,7 @@ import java.util.Optional;
    // Find user by citizenship number (unique, used for login)
     Optional<SimUser> findByCitizenshipNumber(String citizenshipNumber);
         
+        
    // Optionally find by first name (not unique — use carefully)
     Optional<SimUser> findByFirstName(String firstName);
          }
