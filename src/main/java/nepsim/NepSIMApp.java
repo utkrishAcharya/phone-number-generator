68017038dfed79916644da387c0a2429c2200347
utkrishAcharya <acharyautkrish14@gmail.com>
2026-03-13 11:20:17 +0545

    Update NepSIMApp.java

--- src/main/java/nepsim/NepSIMApp.java
+++ src/main/java/nepsim/NepSIMApp.java
@@ -8,5 +8,6 @@ public class NepSIMApp {
  public static void main(String[] args) {
   
         SpringApplication.run(NepSIMApp.class, args);
+  
         System.out.println("NEPSIM Application Started!");
     }}
