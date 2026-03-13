2d6887d1b7c07a10a72ecb97dea2ccb3017bc24c
utkrishAcharya <acharyautkrish14@gmail.com>
2026-03-13 11:20:05 +0545

    Update UserRepository.java

--- src/main/java/nepsim/repository/UserRepository.java
+++ src/main/java/nepsim/repository/UserRepository.java
@@ -6,5 +6,5 @@ import java.util.Optional;
 public interface UserRepository extends MongoRepository<User, String> {
     
     
-    Optional<User> findByUsername(String username);
+     Optional<User> findByUsername(String username);
 }
