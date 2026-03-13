e34d497b8d20f3eeda149e16ca04567e1e1a9d44
utkrishAcharya <acharyautkrish14@gmail.com>
2026-03-13 11:20:43 +0545

    Update JwtRequestFilter.java

--- src/main/java/nepsim/security/JwtRequestFilter.java
+++ src/main/java/nepsim/security/JwtRequestFilter.java
@@ -38,6 +38,7 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 
                 auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(auth);
+                
             }}
 
         chain.doFilter(request, response);
