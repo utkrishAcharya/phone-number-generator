8c3b900a9d99bc1b8ef6c483ce394513b4891d6d
utkrishAcharya <acharyautkrish14@gmail.com>
2026-03-15 21:09:57 +0545

    Update AdminController.java

--- src/main/java/nepsim/controller/AdminController.java
+++ src/main/java/nepsim/controller/AdminController.java
@@ -14,6 +14,7 @@ import java.util.Map;
     public class AdminController {
         
   private final SimUserService simUserService;
+        
 
     @Autowired
     public AdminController(SimUserService simUserService) {
