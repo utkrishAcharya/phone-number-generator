e1385dc499036202f635ddac2ea30ae3ee357565
utkrishAcharya <acharyautkrish14@gmail.com>
2026-03-24 17:51:09 +0545

    Update SecurityConfig.java

--- src/main/java/nepsim/configuration/SecurityConfig.java
+++ src/main/java/nepsim/configuration/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+    
 
     @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
