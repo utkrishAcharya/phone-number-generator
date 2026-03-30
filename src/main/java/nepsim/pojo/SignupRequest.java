759ab89b5c200f48176d993e5e383058f0254aae
utkrishAcharya <acharyautkrish14@gmail.com>
2026-03-30 15:00:29 +0545

    Update SignupRequest.java

--- src/main/java/nepsim/pojo/SignupRequest.java
+++ src/main/java/nepsim/pojo/SignupRequest.java
@@ -6,6 +6,7 @@ public class SignupRequest {
 
     @NotBlank(message = "First name is required")
     private String firstName;
+    
 
     @NotBlank(message = "Last name is required")
     private String lastName;
