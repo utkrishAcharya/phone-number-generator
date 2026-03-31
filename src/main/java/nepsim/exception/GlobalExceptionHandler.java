701d3d21bd83535cd79f51e73aec7c5a40e3924f
utkrishAcharya <acharyautkrish14@gmail.com>
2026-03-31 09:04:50 +0545

    Update GlobalExceptionHandler.java

--- src/main/java/nepsim/exception/GlobalExceptionHandler.java
+++ src/main/java/nepsim/exception/GlobalExceptionHandler.java
@@ -13,6 +13,7 @@ public class GlobalExceptionHandler extends Exception{
          ex.printStackTrace(); // logs the real error in console
         
         
+        
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
              .body("Server error: " + ex.getMessage());
         
