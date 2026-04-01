b517e93c8ff832bba07f6b835eb3d5bc4bb1334a
utkrishAcharya <acharyautkrish14@gmail.com>
2026-04-01 10:37:53 +0545

    Set authentication in security context

--- src/main/java/nepsim/security/JwtRequestFilter.java
+++ src/main/java/nepsim/security/JwtRequestFilter.java
@@ -47,6 +47,7 @@ public class JwtRequestFilter extends OncePerRequestFilter {
                 
 
                 auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                
                 SecurityContextHolder.getContext().setAuthentication(auth);
                 
                 
