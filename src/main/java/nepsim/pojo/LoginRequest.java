74fef61c3e890ab2b0cef801ded757ab4430604c
utkrishAcharya <acharyautkrish14@gmail.com>
2026-04-03 07:52:59 +0545

    Update LoginRequest.java

--- src/main/java/nepsim/pojo/LoginRequest.java
+++ src/main/java/nepsim/pojo/LoginRequest.java
@@ -15,6 +15,7 @@ public class LoginRequest {
 public String getPassword() { return password; }
     
     
+    
     public void setPassword(String password) { this.password = password; }
     
     
