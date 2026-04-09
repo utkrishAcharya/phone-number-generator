a1b2d0305ccc79a9fbc865ba55ff122f99bb6893
utkrishAcharya <acharyautkrish14@gmail.com>
2026-04-09 11:31:18 +0545

    Update SignupRequest.java

--- src/main/java/nepsim/pojo/SignupRequest.java
+++ src/main/java/nepsim/pojo/SignupRequest.java
@@ -41,6 +41,7 @@ public class SignupRequest {
     
     public void setCitizenshipNumber(String citizenshipNumber) { this.citizenshipNumber = citizenshipNumber; }
     
+    
 
     public String getPassword() { return password; }
     
