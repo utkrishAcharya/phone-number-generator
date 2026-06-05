8dbebbfc362ab3f33393164cda48092b4865f3b3
utkrishAcharya <acharyautkrish14@gmail.com>
2026-06-05 09:41:46 +0545

    Update SimUserController.java

--- src/main/java/nepsim/controller/SimUserController.java
+++ src/main/java/nepsim/controller/SimUserController.java
@@ -167,6 +167,7 @@ public class SimUserController {
         if (!deleted) {
             
             return ResponseEntity
+                
                     .status(HttpStatus.NOT_FOUND)
                     .body(Map.of("message", "User not found"));
             
