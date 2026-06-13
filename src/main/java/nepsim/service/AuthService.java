ab291ff20b8ed1edcd47b2657e498bd35dc4e23e
utkrishAcharya <acharyautkrish14@gmail.com>
2026-06-13 06:29:44 +0545

    Update AuthService.java

--- src/main/java/nepsim/service/AuthService.java
+++ src/main/java/nepsim/service/AuthService.java
@@ -74,6 +74,7 @@ public class AuthService {
             
                 passwordEncoder.matches(password, userOpt.get().getPassword())) {
             
+            
             return userOpt;
             
         }
