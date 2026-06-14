5517d1cc5a640add2c4f798e7efb4b167fc68897
utkrishAcharya <acharyautkrish14@gmail.com>
2026-06-14 21:46:13 +0545

    Update SimUserRepository.java

--- src/main/java/nepsim/repository/SimUserRepository.java
+++ src/main/java/nepsim/repository/SimUserRepository.java
@@ -3,6 +3,7 @@ package nepsim.repository;
 import nepsim.model.SimUser;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
 @Repository
