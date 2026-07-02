3407d2a09fc4224fc1b2211a1a3b5b9d9c61d90b
utkrishAcharya <acharyautkrish14@gmail.com>
2026-07-02 20:12:21 +0545

    Remove duplicate findByUsername method declaration

--- src/main/java/nepsim/repository/UserRepository.java
+++ src/main/java/nepsim/repository/UserRepository.java
@@ -5,7 +5,6 @@ import org.springframework.security.core.userdetails.User;
 import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User, String> {
-    
-     Optional<User> findByUsername(String username);
+    Optional<User> findByUsername(String username);
     
      }
