e64659f2f27321d5ea15485b287b3809feee525d
utkrishAcharya <acharyautkrish14@gmail.com>
2026-07-12 21:34:42 +0545

    Update SimUserController.java

--- src/main/java/nepsim/controller/SimUserController.java
+++ src/main/java/nepsim/controller/SimUserController.java
@@ -24,7 +24,6 @@ public class SimUserController {
       @Autowired
     public SimUserController(SimUserService simUserService) {
         
-        
         this.simUserService = simUserService;
         
            }
