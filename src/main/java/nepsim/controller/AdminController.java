a72fd3dce7cc4fc10827026442f38ccf9b6d20f4
utkrishAcharya <acharyautkrish14@gmail.com>
2026-07-21 13:32:38 +0545

    Update AdminController.java

--- src/main/java/nepsim/controller/AdminController.java
+++ src/main/java/nepsim/controller/AdminController.java
@@ -30,6 +30,7 @@ import java.util.Map;
          return ResponseEntity.ok(users);
          
          }
+        
      // Update specific user
     @PutMapping("/users/{id}")
     public ResponseEntity<?> updateUser(@PathVariable String id,
