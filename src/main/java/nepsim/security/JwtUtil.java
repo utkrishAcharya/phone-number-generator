45ed9dc8a5cee5eb8b24f380a53c5e86523a3561
utkrishAcharya <acharyautkrish14@gmail.com>
2026-08-02 08:36:08 +0545

    Remove unnecessary whitespace in JwtUtil constructor

--- src/main/java/nepsim/security/JwtUtil.java
+++ src/main/java/nepsim/security/JwtUtil.java
@@ -20,8 +20,7 @@ public class JwtUtil {
     
 public JwtUtil() {
         
-        
-        // Create a SecretKey from the long secret string
+         // Create a SecretKey from the long secret string
         key = Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
         
         
