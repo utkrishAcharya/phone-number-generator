ee905c5fc1b2776fcdb04637c84bfd8a31ecea19
utkrishAcharya <acharyautkrish14@gmail.com>
2026-08-07 11:56:55 +0545

    Update JwtUtil.java

--- src/main/java/nepsim/security/JwtUtil.java
+++ src/main/java/nepsim/security/JwtUtil.java
@@ -4,7 +4,6 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
-
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
