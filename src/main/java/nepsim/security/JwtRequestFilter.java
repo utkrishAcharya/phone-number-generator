75badd3d57db829acc5995dbc3c3a833331ca073
utkrishAcharya <acharyautkrish14@gmail.com>
2026-08-08 14:16:48 +0545

    Update JwtRequestFilter.java

--- src/main/java/nepsim/security/JwtRequestFilter.java
+++ src/main/java/nepsim/security/JwtRequestFilter.java
@@ -40,6 +40,7 @@ private final JwtUtil jwtUtil;
                         new UsernamePasswordAuthenticationToken(username, null, Collections.emptyList());
                 
                  auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                
                  SecurityContextHolder.getContext().setAuthentication(auth);
                 
                  }}
