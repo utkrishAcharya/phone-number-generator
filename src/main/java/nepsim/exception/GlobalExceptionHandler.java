e167a78d7639d7a6bf82db8a34db7328b193370e
utkrishAcharya <acharyautkrish14@gmail.com>
2026-08-15 19:57:16 +0545

    Update GlobalExceptionHandler.java

--- src/main/java/nepsim/exception/GlobalExceptionHandler.java
+++ src/main/java/nepsim/exception/GlobalExceptionHandler.java
@@ -1,5 +1,4 @@
 package nepsim.exception;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
