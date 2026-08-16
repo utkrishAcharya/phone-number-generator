1b9904602bfc8ecb3599d3169696024272adf1ce
utkrishAcharya <acharyautkrish14@gmail.com>
2026-08-16 10:41:17 +0545

    Add lastName getter and setter methods

--- src/main/java/nepsim/pojo/SignupRequest.java
+++ src/main/java/nepsim/pojo/SignupRequest.java
@@ -27,6 +27,7 @@ public class SignupRequest {
     
     public void setFirstName(String firstName) { this.firstName = firstName; }
     
+    
     public String getLastName() { return lastName; }
     
     public void setLastName(String lastName) { this.lastName = lastName; }
