333cc66be44e7b0fe1a482fd3e154852dc0c8ba5
utkrishAcharya <acharyautkrish14@gmail.com>
2026-08-18 09:00:28 +0545

    Update SignupRequest.java

--- src/main/java/nepsim/pojo/SignupRequest.java
+++ src/main/java/nepsim/pojo/SignupRequest.java
@@ -47,6 +47,7 @@ public class SignupRequest {
     
     public void setFatherName(String fatherName) { this.fatherName = fatherName; }
     
+    
     public String getMotherName() { return motherName; }
     
     public void setMotherName(String motherName) { this.motherName = motherName; }
