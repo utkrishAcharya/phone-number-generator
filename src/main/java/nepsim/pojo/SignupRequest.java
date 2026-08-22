870e6a9d07b0c0cf07861a08aff079bf2e48ddaf
utkrishAcharya <acharyautkrish14@gmail.com>
2026-08-22 22:11:51 +0545

    Add spouse field to SignupRequest class

--- src/main/java/nepsim/pojo/SignupRequest.java
+++ src/main/java/nepsim/pojo/SignupRequest.java
@@ -57,6 +57,7 @@ public class SignupRequest {
     
     public void setPlace(String place) { this.place = place; }
     
+    
     public String getSpouse() { return spouse; }
     
     public void setSpouse(String spouse) { this.spouse = spouse; }
