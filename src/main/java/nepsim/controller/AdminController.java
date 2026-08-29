d93ef51598f688f36d9071e3f4c2285cff49ecf2
utkrishAcharya <acharyautkrish14@gmail.com>
2026-08-29 14:42:17 +0545

    Fix formatting in AdminController.java

--- src/main/java/nepsim/controller/AdminController.java
+++ src/main/java/nepsim/controller/AdminController.java
@@ -13,7 +13,7 @@ import java.util.Map;
     public class AdminController {
 
         
-        private final SimUserService simUserService;
+    private final SimUserService simUserService;
         
     @Autowired
     public AdminController(SimUserService simUserService) {
