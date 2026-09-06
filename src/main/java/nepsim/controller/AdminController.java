e5a470819d415ded2214d7bb26e5777cbf48a443
utkrishAcharya <acharyautkrish14@gmail.com>
2026-09-06 07:52:22 +0545

    Update AdminController.java

--- src/main/java/nepsim/controller/AdminController.java
+++ src/main/java/nepsim/controller/AdminController.java
@@ -37,6 +37,7 @@ import java.util.Map;
     @PutMapping("/users/{id}")
     public ResponseEntity<?> updateUser(@PathVariable String id,
                                         
+                                        
      @RequestBody SimUser simUser) {
         
         
