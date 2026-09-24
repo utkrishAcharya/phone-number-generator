41ce8ff254e31e509b7ea60725ca2f768673f14f
utkrishAcharya <acharyautkrish14@gmail.com>
2026-09-24 21:58:01 +0545

    Update AdminController.java

--- src/main/java/nepsim/controller/AdminController.java
+++ src/main/java/nepsim/controller/AdminController.java
@@ -59,8 +59,7 @@ import java.util.Map;
         
         boolean deleted = simUserService.deleteUser(id);
         
-        
-          if (!deleted) {
+        if (!deleted) {
             
             return ResponseEntity
                 
